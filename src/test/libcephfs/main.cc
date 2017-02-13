9be51046130a6c967f22d21dc8c523707ce1f251
Greg Farnum <gfarnum@redhat.com>
2017-02-12 19:30:34 -0800

    test: do libcephfs chmod as root user
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/test/libcephfs/main.cc
+++ src/test/libcephfs/main.cc
@@ -15,6 +15,7 @@
 
 #include "gtest/gtest.h"
 #include "include/cephfs/libcephfs.h"
+#include "client/UserPerm.h"
 
 static int update_root_mode()
 {
@@ -26,9 +27,19 @@ static int update_root_mode()
   ceph_conf_parse_env(admin, NULL);
   ceph_conf_set(admin, "client_permissions", "false");
   r = ceph_mount(admin, "/");
+
+  // Horrifying hack: we need to chmod as the root user, but don't
+  // have a great way of doing so.
+  UserPerm *mount_perms = ceph_mount_perms(admin);
+  UserPerm existing_perms = *mount_perms;
+  UserPerm root_perms(0, 0);
+
   if (r < 0)
     goto out;
+  
+  *mount_perms = root_perms;
   r = ceph_chmod(admin, "/", 01777);
+  *mount_perms = existing_perms;
 out:
   ceph_shutdown(admin);
   return r;
