843231834bae49e6f9d32621085e9fdd48c2bedd
Greg Farnum <gfarnum@redhat.com>
2020-03-18 03:47:49 +0000

    SQUASH osdmap: add stretch_mode_bucket and Incremental decode fix
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/osd/OSDMap.h
+++ src/osd/OSDMap.h
@@ -366,6 +366,7 @@ public:
     ceph_release_t new_require_osd_release{0xff};
     uint32_t new_stretch_bucket_count{0};
     uint32_t new_degraded_stretch_mode{0};
+    int32_t new_stretch_mode_bucket{0};
     bool stretch_mode_enabled{false};
     bool change_stretch_mode{false};
 
@@ -621,6 +622,7 @@ private:
   bool stretch_mode_enabled; // we are in stretch mode, requiring multiple sites
   uint32_t stretch_bucket_count; // number of sites we expect to be in
   uint32_t degraded_stretch_mode; // 0 if not degraded; else count of up sites
+  int32_t stretch_mode_bucket; // the bucket type we're stretched across
 private:
   uint32_t crush_version = 1;
 
@@ -642,7 +644,7 @@ private:
 	     crc_defined(false), crc(0),
 	     crush(std::make_shared<CrushWrapper>()),
 	     stretch_mode_enabled(false), stretch_bucket_count(0),
-	     degraded_stretch_mode(0) {
+	     degraded_stretch_mode(0), stretch_mode_bucket(0) {
   }
 
 private:
